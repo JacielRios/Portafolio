3669383768fb97ad7f0c62594d8d7feb07c5086e
Jaciel Benito Rios Martinez <jaciel.jbrm@gmail.com>
2023-01-26 15:40:27 -0600

    [ADD] tooltip in technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,46 +1,75 @@
 import React from "react";
-
-import '@styles/Technologies.css';
-import html from '@logos/html5.png';
-import css from '@logos/css3.png';
-import js from '@logos/js.png';
-import react from '@logos/react.png';
-import bootstrap from '@logos/bootstrap.png';
-import laravel from '@logos/laravel.png';
-import git from '@logos/git.png';
+import { styled } from '@mui/material/styles';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { useTranslation } from "react-i18next";
 
+import "@styles/Technologies.css";
+import html from "@logos/html5.png";
+import css from "@logos/css3.png";
+import js from "@logos/js.png";
+import react from "@logos/react.png";
+import bootstrap from "@logos/bootstrap.png";
+import laravel from "@logos/laravel.png";
+import git from "@logos/git.png";
+
+const TechTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} arrow classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.common.black,
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: '600'
+    },
+  }));
 
 const Technologies = () => {
-    const { t } = useTranslation();
-    return(
-        <>
-            <h2 className="title">{t('technologies')}</h2>
-            <section className="container-cards">
-                <div className="card">
-                    <img src={html} alt="Logo html" loading="lazy" />
-                </div>
-                <div className="card">
-                    <img src={css} alt="Logo css" loading="lazy" />
-                </div>
-                <div className="card">
-                    <img src={js} alt="Logo javascript" loading="lazy" />
-                </div>
-                <div className="card">
-                    <img src={react} alt="Logo react" loading="lazy" />
-                </div>
-                <div className="card">
-                    <img src={bootstrap} alt="Logo bootstrap" loading="lazy" />
-                </div>
-                <div className="card">
-                    <img src={laravel} alt="Logo laravel" loading="lazy" />
-                </div>
-                <div className="card">
-                    <img src={git} alt="Logo git" loading="lazy" />
-                </div>
-            </section>
-        </>
-    );
+  const { t } = useTranslation();
+  return (
+    <>
+      <h2 className="title">{t("technologies")}</h2>
+      <section className="container-cards">
+        <div className="card">
+          <TechTooltip  title="HTML" placement="right" arrow>
+            <img src={html} alt="Logo html" loading="lazy" />
+          </TechTooltip >
+        </div>
+        <div className="card">
+          <TechTooltip  title="CSS" placement="right" arrow>
+            <img src={css} alt="Logo css" loading="lazy" />
+          </TechTooltip >
+        </div>
+        <div className="card">
+          <TechTooltip  title="JAVASCRIPT" placement="right" arrow>
+            <img src={js} alt="Logo javascript" loading="lazy" />
+          </TechTooltip >
+        </div>
+        <div className="card">
+          <TechTooltip  title="REACT" placement="right" arrow>
+            <img src={react} alt="Logo react" loading="lazy" />
+          </TechTooltip >
+        </div>
+        <div className="card">
+          <TechTooltip  title="BOOTSTRAP" placement="right" arrow>
+            <img src={bootstrap} alt="Logo bootstrap" loading="lazy" />
+          </TechTooltip >
+        </div>
+        <div className="card">
+          <TechTooltip  title="LARAVEL" placement="right" arrow>
+            <img src={laravel} alt="Logo laravel" loading="lazy" />
+          </TechTooltip >
+        </div>
+        <div className="card">
+          <TechTooltip  title="GIT" placement="right" arrow>
+            <img src={git} alt="Logo git" loading="lazy" />
+          </TechTooltip >
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Technologies;
